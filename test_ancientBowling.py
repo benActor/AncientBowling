5fcfdc5f2b71fd8e329637b684b7d1931a4f5761
benActor <210335@university-365.com>
2021-03-31 11:33:10 +0200

    AncientBowling Test case

--- test_ancientBowling.py
+++ test_ancientBowling.py
@@ -1,8 +1,13 @@
 from unittest import TestCase, main
+from ancientBowling import AncientBowling
 
 
 class AncientBowlingTest(TestCase):
-    pass
+    def setUp(self):
+        self.bowling = AncientBowling()
+
+    def test_bowling_class(self):
+        self.assertIsInstance(self.bowling, AncientBowling)
 
 
 if __name__ == '__main__':
