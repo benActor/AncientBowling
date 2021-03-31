d835a52449323ade0b905734eb4e288a85ad4741
benActor <210335@university-365.com>
2021-03-31 13:28:00 +0200

    run the bowling

--- app.py
+++ app.py
@@ -2,3 +2,19 @@ from ancientBowling import AncientBowling
 
 if __name__ == '__main__':
     bowling = AncientBowling()
+
+    def valid_input(user_input):
+        try:
+            int(user_input)
+            return True
+        except ValueError:
+            return False
+
+    user_input = input("enter the pins Shot: ")
+
+    while True:
+        if not valid_input(user_input):
+            user_input = input("Shot must be integer: ")
+        else:
+            print(f"Your Score is: {bowling.shot(int(user_input))}")
+            user_input = input("enter the pins Shot: ")
