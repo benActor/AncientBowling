f20c505b8adefc55612a1be7af5171417f46dd9c
benActor <210335@university-365.com>
2021-03-31 11:51:17 +0200

    Test for the last frame position

--- test_ancientBowling.py
+++ test_ancientBowling.py
@@ -17,9 +17,20 @@ class AncientBowlingTest(TestCase):
         self.assertTrue(self.bowling.strike_state([15, "-", "-"]))
         self.assertFalse(self.bowling.strike_state([15, "-", "-", "-"]))
 
+    def test_sum_state(self):
+        self.assertEqual(15, self.bowling.sum_state([15, "-", "-"]))
+        self.assertEqual(0, self.bowling.sum_state(["-", "-", "-"]))
+        self.assertEqual(7, self.bowling.sum_state([3, 4, "-"]))
+
     def test_spare_state(self):
         self.assertFalse(self.bowling.spare_state([3, 7, "-"]))
         self.assertTrue(self.bowling.spare_state([8, 7, "-"]))
+
+    def test_last_frame(self):
+        self.assertFalse(self.bowling.last_frame(2, self.bowling.board))
+
+        self.assertTrue(self.bowling.last_frame(4, [[15, "-", "-"], [8, 1, 2], [1, 2, 12], [6, 4, 1],
+                                                    [15, 8, 2, 3]]))
 
 
 if __name__ == '__main__':
