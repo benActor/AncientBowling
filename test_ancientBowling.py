512c6e2cec0a462635fbd0e9af099fabbe6efaea
benActor <210335@university-365.com>
2021-03-31 11:44:38 +0200

    Test for AncientBowling attributes added

--- test_ancientBowling.py
+++ test_ancientBowling.py
@@ -9,6 +9,18 @@ class AncientBowlingTest(TestCase):
     def test_bowling_class(self):
         self.assertIsInstance(self.bowling, AncientBowling)
 
+    def test_create_board(self):
+        self.assertEqual([["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-", "-"]]
+                         , self.bowling.create_board("-"))
+
+    def test_strike_state(self):
+        self.assertTrue(self.bowling.strike_state([15, "-", "-"]))
+        self.assertFalse(self.bowling.strike_state([15, "-", "-", "-"]))
+
+    def test_spare_state(self):
+        self.assertFalse(self.bowling.spare_state([3, 7, "-"]))
+        self.assertTrue(self.bowling.spare_state([8, 7, "-"]))
+
 
 if __name__ == '__main__':
     main()
